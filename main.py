d4334af205f5f73a8c962eb736ea1741c40891a7
Arik <anya.nyuskin2012@mail.ru>
2023-12-19 04:01:25 +0700

    /

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastApi
+from fastapi import FastAPI
 
-app=FastApi()
+app=FastAPI()
 
 @app.get('/')
 async def f_index():
     return{"FIO":"Yudina Anna Gagikovna"}
+
+@app.get('/tools')
+async def f_index1():
+    return{"phone number":"89237517091"}
+
+@app.get('/users')
+async def f_index2():
+    return{"skills":"Well done!"}
